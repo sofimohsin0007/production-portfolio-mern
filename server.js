1c1b6e168f6a00e7f2cf664a65ce73d2b46f578a
mosinf <mosinf@scribd.com>
2025-08-25 14:50:55 +0530

    added new changes

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, "./client/build")));
 //routes
 app.use("/api/v1/portfolio", require("./routes/portfolioRoutes"));
 
-app.get("*", function (req, res) {
+app.get(/(.*)/, function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
