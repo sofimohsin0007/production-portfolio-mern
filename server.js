0c843bd3a18e0d0ffcad138654dd91747272dd9d
mosinf <mosinf@scribd.com>
2025-08-25 13:34:58 +0530

    fixed error

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "./client/build")));
 
 //routes
-app.use("/api/v1/portfolio", require("./routes/portfolioRoute"));
+app.use("/api/v1/portfolio", require("./routes/portfolioRoutes"));
 
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
